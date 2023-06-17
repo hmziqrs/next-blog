40dd3d82a494dafc4557ebfd23117d7163d58ea4
hackerhgl <hackerhgl@gmail.com>
2023-06-17 07:28:17 +0500

    Added matrix background on hero-content

---
+++ app/hero-canvas.tsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+
+const katakana =
+  "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン";
+const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const nums = "0123456789";
+
+const chars = (katakana + latin + nums).split("");
+
+export default function HeroCanvas() {
+  useEffect(() => {
+    const base = document.getElementById("canvas-base") as HTMLDivElement;
+    const canvas = document.getElementById("hero-canvas") as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    canvas.width = base.clientWidth;
+    canvas.height = base.clientHeight;
+
+    const fontSize = 12;
+    const columns = canvas.width / fontSize;
+
+    const drops: number[] = [];
+    for (let x = 0; x < columns; x++) {
+      drops[x] = 1;
+    }
+
+    function draw() {
+      ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      ctx.fillStyle = "#00ff0068";
+      ctx.font = fontSize + "px arial";
+
+      for (let i = 0; i < drops.length; i++) {
+        const text = chars[Math.floor(Math.random() * chars.length)];
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+        if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+          drops[i] = 0;
+        }
+
+        drops[i]++;
+      }
+    }
+
+    setInterval(draw, 33);
+  }, []);
+
+  return (
+    <div id="canvas-base" className="h-full w-full left-0 top-0 absolute z-0">
+      <canvas id="hero-canvas" className="h-full w-full"></canvas>
+    </div>
+  );
+}
