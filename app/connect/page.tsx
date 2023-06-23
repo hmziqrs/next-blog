4bf1f2c639453e2c0a498c7de5fa553b05002180
hackerhgl <hackerhgl@gmail.com>
2023-06-23 21:48:00 +0500

    Shared components extraction

---
+++ app/connect/page.tsx
@@ -0,0 +1,10 @@
+import Container from "components/container";
+
+export default function ConnectPage() {
+  return (
+    <Container>
+      <div className="h-12" />
+      <h1 className="text-3xl text-center">Connect</h1>
+    </Container>
+  );
+}
---
+++ app/lib/links.tsx
@@ -0,0 +1,7 @@
+export const mainSocialLinks = [
+  { href: "https://youtube.com", label: "Youtube" },
+  { href: "https://instagram.com", label: "Instagram" },
+  { href: "https://tiktok.com", label: "Tiktok" },
+  { href: "https://facebook.com", label: "Facebook" },
+  { href: "https://twitter.com", label: "Twitter" },
+];
