3aadeaaba17543463ffba5d9891dae9712a114d9
hackerhgl <hackerhgl@gmail.com>
2023-06-23 19:54:34 +0500

    Added link in footer

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,11 +1,35 @@
+import { cx } from "alias";
 import Container from "components/container";
+import Link from "next/link";
+
+const links = [
+  { href: "/connect", label: "Connect" },
+  { href: "/road-map", label: "Road map" },
+  { href: "/site-map", label: "Site map" },
+];
 
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
     <div className="bg-black py-12">
-      <Container>
-        <p className="text-xl">Copyright {year}</p>
+      <Container className="flex flex-row justify-between">
+        <div>
+          <p className="text-xl">Copyright {year}</p>
+        </div>
+        <div className="flex flex-row space-x-4">
+          {links.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={cx(
+                "underline underline-offset-4",
+                "text-white/80 hover:text-white/100 transition-all"
+              )}
+            >
+              <span>{link.label}</span>
+            </Link>
+          ))}
+        </div>
       </Container>
     </div>
   );
